22abeb40b1212d6944fce4183372a3d56adf26e3
HE1CO <46298915+HE1CO@users.noreply.github.com>
2022-12-08 00:53:15 +0100

    Bug fixes for Invoke/NMKD, added more samplers

--- document.js
+++ document.js
@@ -1,151 +1,176 @@
+// eslint-disable-next-line
 var pngtoy = new PngToy();
 
 function decodeChunkData() {
+    var program;
+    var promptText;
+    var negativePromptText;
+    var seed;
+    var cfg;
+    var steps;
+    var width;
+    var height;
+    var sampler;
     try {
-        var program;
-        var promptText;
-        var negativePromptText;
-        var seed;
-        var cfg;
-        var steps;
-        var width;
-        var height;
-        var sampler;
         const textChunk = pngtoy.getChunk("tEXt");
-        if (!textChunk) { return; }
-        const keyWord = textChunk[0].keyword;
-        var keyText = textChunk[0].text;
-        if (keyWord == 'parameters') {
-            // Extract values from SD webUI image
-            program = 'sdWebUi';
+        if (!textChunk) return;
+        var keyText;
+        if (textChunk.length > 1) {
+            if (textChunk[1].keyword == "sd-metadata") { program = "invoke"; } // prettier-ignore
+            keyText = textChunk[1].text;
+        } else if (textChunk.length == 1) {
+            if (textChunk[0].keyword == "parameters") { program = "sdWebUi"; } // prettier-ignore
+            keyText = textChunk[0].text;
+        }
+        if (!program) return;
+        if (program == "sdWebUi") {
             var separateLines = keyText.split(/\r?\n|\r|\n/g);
             promptText = separateLines[0];
-            if (separateLines[1].startsWith('Negative prompt:')) {
+            if (separateLines[1].startsWith("Negative prompt:")) {
                 negativePromptText = separateLines[1].substring(16).trim();
             }
-            const settingsTextIndex = (typeof negativePromptText !== 'undefined') ? 2 : 1;
-            const settingsTextArray = separateLines[settingsTextIndex].split(',');
+            const settingsTextIndex = negativePromptText ? 2 : 1;
+            const settingsTextArray = separateLines[settingsTextIndex].split(",");
             var settingsDict = {};
-            settingsTextArray.forEach(element => {
-                const list = element.split(':');
-                settingsDict[String(list[0]).trim().replace(/\s/g, '')] = String(list[1]).trim();
+            settingsTextArray.forEach((element) => {
+                const list = element.split(":");
+                settingsDict[String(list[0]).trim().replace(/\s/g, "")] = String(list[1]).trim();
             });
             seed = settingsDict.Seed;
             cfg = settingsDict.CFGscale;
             steps = settingsDict.Steps;
-            width = settingsDict.Size.split('x')[0];
-            height = settingsDict.Size.split('x')[1];
+            width = settingsDict.Size.split("x")[0];
+            height = settingsDict.Size.split("x")[1];
             sampler = settingsDict.Sampler;
-        } else if (keyWord == 'Dream') {
-            // Extract values from NMKD image
-            program = 'nmkd';
-            keyText = keyText.replace(/["]+/g, '') // Fix for old NMKD versions
-            const rawPromptText = keyText.substring(0, keyText.indexOf(' -s '));
+        }
+        if (program == "invoke") {
+            const json = JSON.parse(keyText);
+            const rawPromptText = json.image.prompt[0].prompt;
             const amountOfBrackets = (rawPromptText.match(/\[|\]/g) || []).length;
-            if (amountOfBrackets != 0 && amountOfBrackets != 2) { return; }
+            if (amountOfBrackets != 0 && amountOfBrackets != 2) return;
             const regexNegativePromptMatch = rawPromptText.match(/\[([^)]+)\]/);
             if (regexNegativePromptMatch) {
                 negativePromptText = regexNegativePromptMatch[1];
-                promptText = rawPromptText.replace(regexNegativePromptMatch[0],'').replace(/,\s*$/,'');
+                promptText = rawPromptText.replace(regexNegativePromptMatch[0], "").replace(/,\s*$/, "");
             } else {
-                promptText = rawPromptText.replace(/,\s*$/,'');
+                promptText = rawPromptText.replace(/,\s*$/, "");
             }
-            const settingsText = keyText.replace(rawPromptText,'');
-            seed = settingsText.match(/-S(.*)-W/)[1].trim();
-            cfg = settingsText.match(/-C(.*?)--/)[1].trim();
-            steps = settingsText.match(/-s(.*)-S/)[1].trim();
-            width = settingsText.match(/-W(.*)-H/)[1].trim();
-            height = settingsText.match(/-H(.*)-C/)[1].trim();
-            sampler = settingsText.match(/-A(.*)-U/)[1].trim();
+            seed = json.image.seed;
+            cfg = json.image.cfg_scale;
+            steps = json.image.steps;
+            width = json.image.width;
+            height = json.image.height;
+            sampler = json.image.sampler;
         }
-        // console.log('### program:', program);
-        // console.log('### promptText:', promptText);
-        // console.log('### negativePromptText:', negativePromptText);
-        // console.log('### seed:', seed);
-        // console.log('### cfg:', cfg);
-        // console.log('### steps:', steps);
-        // console.log('### width:', width);
-        // console.log('### height:', height);
-        // console.log('### sampler:', sampler);
+        // console.log("### program:", program);
+        // console.log("### promptText:", promptText);
+        // console.log("### negativePromptText:", negativePromptText);
+        // console.log("### seed:", seed);
+        // console.log("### cfg:", cfg);
+        // console.log("### steps:", steps);
+        // console.log("### width:", width);
+        // console.log("### height:", height);
+        // console.log("### sampler:", sampler);
         fillUploadForm(program, promptText, negativePromptText, seed, cfg, steps, width, height, sampler);
     } catch (error) {
-        console.log('Error:', error);
+        console.log("Error:", error);
     }
 }
 
-function readImageFile(e) {
-    function error(err) {console.log('Error. Could not load PNG file:', err)}
+function readImageFile() {
+    function error(err) {
+        console.log("Error. Could not load PNG file:", err);
+    }
     var fr = new FileReader();
-    fr.onload = function() {
+    fr.onload = function () {
         var buffer = this.result;
         pngtoy.fetch(buffer).then(decodeChunkData, error);
     };
-    fr.readAsArrayBuffer(this.files[0])
+    fr.readAsArrayBuffer(this.files[0]);
 }
 
 function fillUploadForm(program, promptText, negativePromptText, seed, cfg, steps, width, height, sampler) {
-  sdWebUiSamplerDict = {
-    ddim: 'DDIM',
-    plms: 'PLMS',
-    k_euler: 'Euler',
-    k_euler_ancestral: 'Euler a',
-    k_heun: 'Heun',
-    k_dpm_2: 'DPM2',
-    k_dpm_2_ancestral: 'DPM2 a',
-    k_lms: 'LMS'
-  }
-  nmkdSamplerDict = {
-    ddim: 'ddim',
-    plms: 'plms',
-    k_euler: 'k_euler',
-    k_euler_ancestral: 'k_euler_a',
-    k_heun: 'k_heun',
-    k_dpm_2: 'k_dpm_2',
-    k_dpm_2_ancestral: 'k_dpm_2_a',
-    k_lms: 'k_lms'
-  }
-  document.getElementById('prompt_prompt').value = promptText;
-  if (negativePromptText) {
-    document.getElementById('prompt_negative_prompt').value = negativePromptText;
-  } else {
-    document.getElementById('prompt_negative_prompt').value = '';
-  }
-  document.getElementById('prompt_metadata_seed').value = seed;
-  document.getElementById('prompt_metadata_guidance_scale').value = cfg;
-  document.getElementById('prompt_metadata_steps').value = steps;
-  document.getElementById('prompt_metadata_width').value = width;
-  document.getElementById('prompt_metadata_height').value = height;
-  var samplerOption;
-  if (program == 'sdWebUi') {
-    samplerOption = Object.keys(sdWebUiSamplerDict).find(key => sdWebUiSamplerDict[key] === sampler);
-  } else if (program == 'nmkd') {
-    samplerOption = Object.keys(nmkdSamplerDict).find(key => nmkdSamplerDict[key] === sampler);
-  }
-  if (samplerOption) {
-    document.getElementById('prompt_metadata_sampler').value = samplerOption;
-  } else {
-    document.getElementById('prompt_metadata_sampler').value = '';
-  }
-  var negativePromptElement = document.getElementById('negative-prompt-collapse');
-  var otherMetadataElement = document.getElementById('other-metadata-collapse');
-  if (negativePromptElement && otherMetadataElement) {
-    negativePromptElement.classList.add('show');
-    otherMetadataElement.classList.add('show');
-  }
+    const sdWebUiSamplersDict = {
+        ddim: "DDIM",
+        plms: "PLMS",
+        k_euler: "Euler",
+        k_euler_ancestral: "Euler a",
+        k_heun: "Heun",
+        k_dpm_2: "DPM2",
+        k_dpm_2_ancestral: "DPM2 a",
+        k_lms: "LMS",
+        k_dpmpp_2: "DPM++ 2M",
+        k_dpmpp_2_ancestral: "DPM++ 2S a",
+        k_dpm_fast: "DPM fast",
+        k_dpm_adaptive: "DPM adaptive",
+        k_lms_karras: "LMS Karras",
+        k_dpm_2_karras: "DPM2 Karras",
+        k_dpm_2_ancestral_karras: "DPM2 a Karras",
+        k_dpmpp_2_karras: "DPM++ 2M Karras",
+        k_dpmpp_2_ancestral_karras: "DPM++ 2S a Karras",
+    };
+    const invokeSamplersDict = {
+        ddim: "ddim",
+        plms: "plms",
+        k_euler: "k_euler",
+        k_euler_ancestral: "k_euler_a",
+        k_heun: "k_heun",
+        k_dpm_2: "k_dpm_2",
+        k_dpm_2_ancestral: "k_dpm_2_a",
+        k_lms: "k_lms",
+        k_dpmpp_2: "k_dpmpp_2",
+        k_dpmpp_2_ancestral: "k_dpmpp_2_a",
+    };
+    document.getElementById("prompt_prompt").value = promptText;
+    if (negativePromptText) {
+        document.getElementById("prompt_negative_prompt").value = negativePromptText;
+    } else {
+        document.getElementById("prompt_negative_prompt").value = "";
+    }
+    document.getElementById("prompt_metadata_seed").value = seed;
+    document.getElementById("prompt_metadata_guidance_scale").value = cfg;
+    document.getElementById("prompt_metadata_steps").value = steps;
+    document.getElementById("prompt_metadata_width").value = width;
+    document.getElementById("prompt_metadata_height").value = height;
+    var samplerOption;
+    if (program == "sdWebUi") {
+        samplerOption = Object.keys(sdWebUiSamplersDict).find((key) => sdWebUiSamplersDict[key] === sampler);
+    } else if (program == "invoke") {
+        samplerOption = Object.keys(invokeSamplersDict).find((key) => invokeSamplersDict[key] === sampler);
+    }
+    if (samplerOption) {
+        if (samplerOption == "k_dpmpp_2") {
+            document.getElementById("prompt_metadata_sampler").value = "dpm++_2";
+        } else if (samplerOption == "k_dpmpp_2_ancestral") {
+            document.getElementById("prompt_metadata_sampler").value = "dpm++_2_ancestral";
+        } else {
+            document.getElementById("prompt_metadata_sampler").value = samplerOption;
+        }
+    } else {
+        document.getElementById("prompt_metadata_sampler").value = "";
+    }
+    var negativePromptElement = document.getElementById("negative-prompt-collapse");
+    var otherMetadataElement = document.getElementById("other-metadata-collapse");
+    if (negativePromptElement && otherMetadataElement) {
+        negativePromptElement.classList.add("show");
+        otherMetadataElement.classList.add("show");
+    }
 }
 
 (function () {
-  const addListener = () => {
-    if (window.location.href == 'https://prompthero.com/prompt/upload') {
-      try {
-        const imageUploadField = document.getElementById('prompt_main_image');
-        if (!imageUploadField.hasAttribute('extension-autofill-prompthero-uploads')) {
-          document.getElementById('prompt_main_image').addEventListener('change', readImageFile, false);
-          console.log('Added event listener hashchange');
-          imageUploadField.setAttribute('extension-autofill-prompthero-uploads', true)
+    const addListener = () => {
+        if (window.location.href == "https://prompthero.com/prompt/upload") {
+            try {
+                const imageUploadField = document.getElementById("prompt_main_image");
+                if (!imageUploadField.hasAttribute("extension-autofill-prompthero-uploads")) {
+                    document.getElementById("prompt_main_image").addEventListener("change", readImageFile, false);
+                    // console.log("Added event listener hashchange");
+                    imageUploadField.setAttribute("extension-autofill-prompthero-uploads", true);
+                }
+            } catch (error) {
+                return;
+            }
         }
-      } catch (error) { return; }
-    }
-  };
-  setInterval(addListener, 200);
+    };
+    setInterval(addListener, 200);
 })();
