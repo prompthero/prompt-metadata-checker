434c533c6ec31b4041be0d7cc4a1fd2aec5af97c
Javi R <4920956+rameerez@users.noreply.github.com>
2023-03-10 19:00:35 +0000

    Add Heico's old code to verify setup works

--- src/controllers/prompts/upload_controller.js
+++ src/controllers/prompts/upload_controller.js
@@ -5,12 +5,62 @@ import FilePondPluginImageResize from 'filepond-plugin-image-resize';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 
-
+PngToy = window.PngToy
+import PromptInfo from "vendor/he1co_postie/prompt_info";
 export default class extends Controller {
   static targets = [ "prompt", "negativePrompt", "modelUsed", "modelVersion", "sampler", "seed", "guidance_scale", "steps", "width", "height", "modelHash", "programUsed" ]
 
   initialize() {
-    console.log("asd")
+    FilePond.registerPlugin(FilePondPluginImageResize, FilePondPluginImagePreview, FilePondPluginFileValidateType);
+    const inputElement = document.getElementById("filepond-input");
+    const pond = FilePond.create(inputElement, {
+      required: true,
+      acceptedFileTypes: ['image/*'],
+      multiple: false,
+      imageResizeMode: 'contain',
+      allowImagePreview: true,
+      allowProcess: true,
+      storeAsFile: true,
+      imagePreviewMinHeight: 600,
+      imagePreviewMaxHeight: 800,
+      imagePreviewMarkupShow: false
+    });
+
+    pond.on('addfile', (error, file) => {
+      if (error) {
+        console.error('Oh no! Error loading image.');
+        return;
+      }
+
+      var pngtoy = new PngToy();
+
+      var fr = new FileReader();
+
+      var that = this;
+
+      fr.onload = function () {
+
+        var buffer = this.result;
+
+        pngtoy.fetch(buffer).then( () => {
+          var promptInfo = new PromptInfo();
+
+          promptInfo.decodeExif(pngtoy);
+
+          if (!promptInfo.prompt){
+            console.warn("No prompt metadata found, leaving form as is...")
+          }
+          else {
+            console.log("Prompt metadata found. Filling up form...")
+            that.fillUpFormWithPromptInfo(promptInfo);
+          }
+        }, (e) => { console.error('Error decoding metadata with PngToy', e) });
+
+      };
+
+      fr.readAsArrayBuffer(file.file);
+
+    });
   }
 
   fillUpFormWithPromptInfo(promptInfo){
